1ff1ad90b289150760fefc9881e646520ee7b58c
ktaga <taga@issp.u-tokyo.ac.jp>
2021-08-23 00:14:44 +0900

    moving amount colculation update

--- rhk_interface/plotter/tools.py
+++ rhk_interface/plotter/tools.py
@@ -50,15 +50,26 @@ def moving_amount(image1, image2):
 
     move_list = []
 
-    for item in matches[:5]:
+    best_match_std = 1
+    for i, item in enumerate(matches):
         pt2 = np.array([kp2[item.trainIdx].pt[0], kp2[item.trainIdx].pt[1]])
         pt1 = np.array([kp1[item.queryIdx].pt[0], kp1[item.queryIdx].pt[1]])
         move = pt2 - pt1
         move[1] = -move[1]  # fix image flipping
         move_list.append(move)
+        match_std = np.array(move_list).std(axis=0).mean()
+        if i == 0:
+            valild_match_num = 1
+        elif match_std > best_match_std:
+            break
+        else:
+            valild_match_num = i + 1
+            best_match_std = match_std
 
-    out = cv2.drawMatches(image1, kp1, image2, kp2, matches[:5], None, flags=2)
+    move_array = np.array(move_list)[:valild_match_num]
+    out = cv2.drawMatches(image1, kp1, image2, kp2, matches[:valild_match_num], None, flags=2)
     plt.imshow(out), plt.show()
-    move_index = np.array(move_list).mean(axis=0)
+    move_index = move_array.mean(axis=0)
+    move_index_std = move_array.std(axis=0)
     delta = np.array([(x_array.max() - x_array.min()) / x_array.shape[0], (y_array.max() - y_array.min()) / y_array.shape[0]])
-    return delta * move_index
+    return delta * move_index, delta * move_index_std
